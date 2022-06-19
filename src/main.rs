25340e9f1ce48de941cbee1793746356618cb22d
devj <power3xz@gmail.com>
2022-06-19 10:35:54 +0900

    프레임 속도 수정

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use bracket_lib::prelude::*;
 
 const SCREEN_WIDTH: i32 = 80;
 const SCREEN_HEIGHT: i32 = 50;
-const FRAME_DURATION: f32 = 75.0;
+const FRAME_DURATION: f32 = 20.0;
 
 struct State {
     player: Player,
