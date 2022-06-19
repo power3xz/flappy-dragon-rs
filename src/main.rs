294d7827a3f0535ef555ba665fa74b41c4181e04
devj <power3xz@gmail.com>
2022-06-19 09:37:50 +0900

    State 구조체 Player 멤버 추가

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 use bracket_lib::prelude::*;
 
 struct State {
+    player: Player,
+    frame_time: f32,
     mode: GameMode,
 }
 
 impl State {
     fn new() -> Self {
         State {
+            player: Player::new(5, 25),
+            frame_time: 0.0,
             mode: GameMode::Menu,
         }
     }
@@ -15,6 +19,8 @@ impl State {
     }
 
     fn restart(&mut self) {
+        self.player = Player::new(5, 25);
+        self.frame_time = 0.0;
         self.mode = GameMode::Playing;
     }
 
