7f51cd125651ec6346554f0f3ff40a567a6aaffb
devj <power3xz@gmail.com>
2022-06-19 10:17:46 +0900

    Obstacle 구조체 추가

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,9 @@ const FRAME_DURATION: f32 = 75.0;
 struct State {
     player: Player,
     frame_time: f32,
+    obstacle: Obstacle,
     mode: GameMode,
+    score: i32,
 }
 
 impl State {
@@ -15,7 +17,9 @@ impl State {
         State {
             player: Player::new(5, 25),
             frame_time: 0.0,
+            obstacle: Obstacle::new(SCREEN_WIDTH, 0),
             mode: GameMode::Menu,
+            score: 0,
         }
     }
     fn play(&mut self, ctx: &mut BTerm) {
@@ -23,7 +27,7 @@ impl State {
         self.frame_time += ctx.frame_time_ms;
         if self.frame_time > FRAME_DURATION {
             self.frame_time = 0.0;
-            self.player.gravity_and__move()
+            self.player.gravity_and_move()
         }
         if let Some(VirtualKeyCode::Space) = ctx.key {
             self.player.flap();
@@ -106,7 +110,7 @@ impl Player {
         ctx.set(0, self.y, YELLOW, BLACK, to_cp437('@'))
     }
 
-    fn gravity_and__move(&mut self) {
+    fn gravity_and_move(&mut self) {
         if self.velocity < 2.0 {
             self.velocity += 0.2;
         }
@@ -119,6 +123,44 @@ impl Player {
 
     fn flap(&mut self) {
         self.velocity = -2.0;
+    }
+}
+
+struct Obstacle {
+    x: i32,
+    gap_y: i32,
+    size: i32,
+}
+
+impl Obstacle {
+    fn new(x: i32, score: i32) -> Self {
+        let mut random = RandomNumberGenerator::new();
+        Obstacle {
+            x,
+            gap_y: random.range(10, 40),
+            size: i32::max(2, 20 - score),
+        }
+    }
+
+    fn render(&mut self, ctx: &mut BTerm, player_x: i32) {
+        let screen_x = self.x - player_x;
+        let half_size = self.size / 2;
+
+        for y in 0..self.gap_y - half_size {
+            ctx.set(screen_x, y, RED, BLACK, to_cp437('|'))
+        }
+
+        for y in self.gap_y + half_size..SCREEN_HEIGHT {
+            ctx.set(screen_x, y, RED, BLACK, to_cp437('|'))
+        }
+    }
+
+    fn hit_obstacle(&self, player: &Player) -> bool {
+        let half_size = self.size / 2;
+        let does_x_match = player.x == self.x;
+        let player_above_gap = player.y < self.gap_y - half_size;
+        let player_below_gap = player.y > self.gap_y + half_size;
+        does_x_match && (player_above_gap || player_below_gap)
     }
 }
 
